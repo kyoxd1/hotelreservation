309b4b2f7484d6e46ba2758b7c371dbd4d3874f6
kyoxd1 <kyomaster1234@gmail.com>
2022-05-08 21:22:42 -0400

    routesForApp

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Header from './components/Header';
 import Home from './components/Home';
 import SearchPage from './components/SearchPage';
 import { Counter } from './features/counter/Counter';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header/>
-      <Home/>
-      <SearchPage/>
-    </div>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/search" element={<SearchPage/>}/>
+      </Routes>
+    </Router>
   );
 }
 
